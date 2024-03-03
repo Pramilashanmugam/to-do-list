88743152e1ed2b2960f90c7fa1f6fc799d4d72ab
Pramila shanmugam <pramila.sl@gmail.com>
2024-03-03 19:44:24 +0000

    add_task function and date validation function using library created

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-print("""▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+from datetime import datetime
+
+print("""\n▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
 ▐  ███████████                 █████             ████   ███           █████    ▌
 ▐ ░█░░░███░░░█                ░░███             ░░███  ░░░           ░░███     ▌
 ▐ ░   ░███  ░   ██████      ███████   ██████     ░███  ████   █████  ███████   ▌
@@ -9,11 +11,11 @@ print("""▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀�
 ▐    ░░░░░     ░░░░░░      ░░░░░░░░  ░░░░░░     ░░░░░ ░░░░░ ░░░░░░     ░░░░░   ▌
 ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌""")
 print ("\nWelcome to your To do list app!")
-print ("This app helps you to add, delete, view and modify your things to do on daily life \n")
-print ()
+print ("This app helps you to keep track of your day to day activities.\n")
+print ("You can add, delete, view or modify your tasks in this app\n")
 
 
-task = ""
+"""tasks = []
 #while True:
     print("\nOptions:")
     print("1. Add Task")
@@ -21,8 +23,35 @@ task = ""
     print("3. View Tasks")
     print("4. Delete Task")
     print("5. Exit")
-    choice = input("Enter your choice: ")
+    choice = input("Enter your choice: ")"""
+
+def validate_date(date_str):
+    """ 
+    This function is to validate the date format entered by user is valid or not
+    """
+    try:
+        datetime.strptime(date_str, '%d/%m/%Y')
+        return True
+    except ValueError:
+        return False
 
 def add_task():
+    """
+    A function is to add task by the user with the date for completion, I have used a loop to ensure the user enters the valid date in the format of DD/MM/YYYY
+    """
+    # Asking user to enter the task
+    task = input("Please add enter your task:")
+    # Checking for valid date and loops until valid date is received
+    while True:
+        date = input("Please enter the date for completion(dd/mm/yyyy):")
+        # Validate date
+        if validate_date(date):
+            print("")
+            break
+        else:
+            print("Invalid date format. Please enter date in dd/mm/yyyy format.")
+
+   
+add_task()
 
 
